6475a34fcb68fb893826d2dab221585543a60880
Rogelio Guzman <rogelioguzmanh@gmail.com>
2017-09-10 10:09:58 -0700

    Use cli.isPathIgnore

--- src/runESLint.js
+++ src/runESLint.js
@@ -1,89 +1,85 @@
 const getLocalESLint = require('./utils/getLocalESLint');
 const toTestResult = require('./utils/toTestResult');
 
-const isFileIgnored = ({ results, warningCount, errorCount }) => {
-  return (
-    warningCount === 1 &&
-    errorCount === 0 &&
-    results &&
-    results[0] &&
-    results[0].messages[0].message.includes('Use "--no-ignore" to override.')
-  );
-};
+const skip = ({ start, end, testPath }) =>
+  toTestResult({
+    stats: {
+      failures: 0,
+      pending: 1,
+      passes: 0,
+      start,
+      end,
+    },
+    skipped: true,
+    tests: [
+      {
+        duration: end - start,
+        testPath,
+      },
+    ],
+    jestTestPath: testPath,
+  });
+
+const fail = ({ start, end, testPath, errorMessage }) =>
+  toTestResult({
+    errorMessage,
+    stats: {
+      failures: 1,
+      pending: 0,
+      passes: 0,
+      start,
+      end,
+    },
+    tests: [
+      {
+        duration: end - start,
+        testPath,
+        errorMessage,
+      },
+    ],
+    jestTestPath: testPath,
+  });
+
+const pass = ({ start, end, testPath }) =>
+  toTestResult({
+    stats: {
+      failures: 0,
+      pending: 0,
+      passes: 1,
+      start,
+      end,
+    },
+    tests: [
+      {
+        duration: end - start,
+        testPath,
+      },
+    ],
+    jestTestPath: testPath,
+  });
 
 const runESLint = ({ testPath, config }, workerCallback) => {
   try {
     const start = +new Date();
     const { CLIEngine } = getLocalESLint(config);
     const cli = new CLIEngine({});
-    const report = cli.executeOnFiles([testPath]);
-    const end = +new Date();
-
-    const formatter = cli.getFormatter();
-    if (isFileIgnored(report)) {
-      workerCallback(
-        null,
-        toTestResult({
-          stats: {
-            failures: 0,
-            pending: 1,
-            passes: 0,
-            start,
-            end,
-          },
-          skipped: true,
-          tests: [
-            {
-              duration: end - start,
-              testPath,
-            },
-          ],
-          jestTestPath: testPath,
-        }),
-      );
-    } else if (report.errorCount > 0) {
-      const errorMessage = formatter(CLIEngine.getErrorResults(report.results));
-      workerCallback(
-        null,
-        toTestResult({
-          errorMessage,
-          stats: {
-            failures: 1,
-            pending: 0,
-            passes: 0,
-            start,
-            end,
-          },
-          tests: [
-            {
-              duration: end - start,
-              testPath,
-              errorMessage,
-            },
-          ],
-          jestTestPath: testPath,
-        }),
-      );
+    if (cli.isPathIgnored(testPath)) {
+      const end = +new Date();
+      workerCallback(null, skip({ start, end, testPath }));
     } else {
-      workerCallback(
-        null,
-        toTestResult({
-          stats: {
-            failures: 0,
-            pending: 0,
-            passes: 1,
-            start,
-            end,
-          },
-          tests: [
-            {
-              duration: end - start,
-              testPath,
-            },
-          ],
-          jestTestPath: testPath,
-        }),
-      );
+      const report = cli.executeOnFiles([testPath]);
+      const end = +new Date();
+
+      if (report.errorCount > 0) {
+        const formatter = cli.getFormatter();
+        const errorMessage = formatter(
+          CLIEngine.getErrorResults(report.results),
+        );
+
+        workerCallback(null, fail({ start, end, testPath, errorMessage }));
+      } else {
+        workerCallback(null, pass({ start, end, testPath }));
+      }
     }
   } catch (e) {
     workerCallback(e);
