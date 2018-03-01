720d83eeaf8815a2ea926a2c2d07988ed93d63b3
Min Idzelis <midzelis@salesforce.com>
2018-03-01 10:45:11 -0500

    Use env var to override local eslint location

--- src/utils/getLocalESLint.js
+++ src/utils/getLocalESLint.js
@@ -2,6 +2,10 @@ const path = require('path');
 const findUp = require('find-up');
 
 const getLocalESLint = config => {
+  if (process.env.ESLINT_PATH) {
+    // eslint-disable-next-line import/no-dynamic-require,global-require
+    return require(process.env.ESLINT_PATH);
+  }
   const nodeModulesPath = findUp.sync('node_modules', { cwd: config.rootDir });
   // eslint-disable-next-line import/no-dynamic-require, global-require
   return require(path.resolve(nodeModulesPath, 'eslint'));
