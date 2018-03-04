3aea76d91d50e2185eadea7d6715f7bf64ec8d1b
Min Idzelis <midzelis@salesforce.com>
2018-03-03 19:48:18 -0500

    Fallback to normal require() if can't find local ESLint.

--- src/utils/getLocalESLint.js
+++ src/utils/getLocalESLint.js
@@ -2,13 +2,17 @@ const path = require('path');
 const findUp = require('find-up');
 
 const getLocalESLint = config => {
-  if (process.env.ESLINT_PATH) {
-    // eslint-disable-next-line import/no-dynamic-require,global-require
-    return require(process.env.ESLINT_PATH);
+  var nodeModulesPath = findUp.sync('node_modules', { cwd: config.rootDir });
+  try {
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    return require(path.resolve(nodeModulesPath, 'eslint'));
+  } catch(e) {
+    // try resolving eslint using normal eslint resolution mechanism
+    try {
+      return require('eslint');
+    } catch(ee) {}
+    throw e;
   }
-  const nodeModulesPath = findUp.sync('node_modules', { cwd: config.rootDir });
-  // eslint-disable-next-line import/no-dynamic-require, global-require
-  return require(path.resolve(nodeModulesPath, 'eslint'));
 };
 
 module.exports = getLocalESLint;
