1ead5a3dd9a8ec812ce6350940a6e622844146b5
Min Idzelis <midzelis@salesforce.com>
2018-03-03 20:03:16 -0500

    Fix lint

--- src/utils/getLocalESLint.js
+++ src/utils/getLocalESLint.js
@@ -2,15 +2,18 @@ const path = require('path');
 const findUp = require('find-up');
 
 const getLocalESLint = config => {
-  var nodeModulesPath = findUp.sync('node_modules', { cwd: config.rootDir });
+  const nodeModulesPath = findUp.sync('node_modules', { cwd: config.rootDir });
   try {
     // eslint-disable-next-line import/no-dynamic-require, global-require
     return require(path.resolve(nodeModulesPath, 'eslint'));
-  } catch(e) {
+  } catch (e) {
     // try resolving eslint using normal eslint resolution mechanism
     try {
+      // eslint-disable-next-line global-require
       return require('eslint');
-    } catch(ee) {}
+    } catch (ee) {
+      // ignore
+    }
     throw e;
   }
 };
