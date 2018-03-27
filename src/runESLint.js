4eb2f0f432aa4c27a558a03648685f2596814480
Simen Bekkhus <sbekkhus91@gmail.com>
2018-03-27 19:09:29 +0200

    return from runner

--- src/runESLint.js
+++ src/runESLint.js
@@ -7,7 +7,7 @@ const fail = createJestRunner.fail;
 const skip = createJestRunner.skip;
 
 const runESLint = ({ testPath, config }) => {
-  const start = +new Date();
+  const start = Date.now();
 
   if (config.setupTestFrameworkScriptFile) {
     // eslint-disable-next-line import/no-dynamic-require,global-require
@@ -19,25 +19,25 @@ const runESLint = ({ testPath, config }) => {
   const cli = new CLIEngine(options.cliOptions);
   if (cli.isPathIgnored(testPath)) {
     const end = +new Date();
-    skip({ start, end, testPath });
-  } else {
-    const report = cli.executeOnFiles([testPath]);
-
-    if (options.cliOptions && options.cliOptions.fix) {
-      CLIEngine.outputFixes(report);
-    }
-
-    const end = +new Date();
-
-    if (report.errorCount > 0) {
-      const formatter = cli.getFormatter(options.cliOptions.format);
-      const errorMessage = formatter(CLIEngine.getErrorResults(report.results));
-
-      fail({ start, end, testPath, errorMessage });
-    } else {
-      pass({ start, end, testPath });
-    }
+    return skip({ start, end, testPath });
   }
+
+  const report = cli.executeOnFiles([testPath]);
+
+  if (options.cliOptions && options.cliOptions.fix) {
+    CLIEngine.outputFixes(report);
+  }
+
+  const end = Date.now();
+
+  if (report.errorCount > 0) {
+    const formatter = cli.getFormatter(options.cliOptions.format);
+    const errorMessage = formatter(CLIEngine.getErrorResults(report.results));
+
+    return fail({ start, end, testPath, errorMessage });
+  }
+
+  return pass({ start, end, testPath });
 };
 
 module.exports = runESLint;
