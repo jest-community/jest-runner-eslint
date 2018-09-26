c149e4bc46a93f359582dc582e29231653c2d6f0
Rogelio Guzman <rogelioguzmanh@gmail.com>
2018-09-26 07:43:10 -0700

    Add jest-watch-typeahead and split use multi project in jest config

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,18 @@ module.exports = {
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
   ],
-  testPathIgnorePatterns: ['/examples/', '/node_modules/', '/__eslint__/'],
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.js',
-    '<rootDir>/integrationTests/*.test.js',
-    '<rootDir>/integrationTests/**/*.test.js',
+  projects: [
+    {
+      displayName: 'e2e',
+      testPathIgnorePatterns: ['/examples/', '/node_modules/', '/__eslint__/'],
+      testMatch: [
+        '<rootDir>/integrationTests/*.test.js',
+        '<rootDir>/integrationTests/**/*.test.js',
+      ],
+    },
+    {
+      displayName: 'tests',
+      testMatch: ['<rootDir>/src/**/__tests__/**/*.js'],
+    },
   ],
 };
