fc4164591a3f660c33c7fbca2b10820d5c64c8b4
Rogelio Guzman <rogelioguzmanh@gmail.com>
2018-09-27 16:35:18 -0700

    Better UX

--- src/watchFixPlugin/index.js
+++ src/watchFixPlugin/index.js
@@ -1,15 +1,51 @@
+const chalk = require('chalk');
+const ora = require('ora');
 const configOverrides = require('../utils/configOverrides');
 
+const wait = time => new Promise(res => setTimeout(res, time));
+
 class ESLintWatchFixPlugin {
-  run() {
-    configOverrides.setFix(!configOverrides.getFix());
-    return Promise.resolve(true);
+  constructor({ stdout, config }) {
+    this._stdout = stdout;
+    this._key = config.key || 'F';
   }
+
+  // eslint-disable-next-line class-methods-use-this
+  async run() {
+    const fix = configOverrides.getFix();
+    configOverrides.setFix(!fix);
+    await this._showLoading(fix);
+    return true;
+  }
+
   getUsageInfo() {
     return {
-      key: 'F',
-      prompt: `${configOverrides.getFix() ? 'disable' : 'enable'} ESLint --fix`,
+      key: this._key,
+      prompt: this._getPrompt(),
     };
+  }
+
+  async _showLoading(fix) {
+    this._stdout.write('\n');
+    const spinner = ora({
+      text: chalk.italic.dim(`${fix ? 'Disabling' : 'Enabling'} ESLint --fix`),
+      stream: this._stdout,
+    }).start();
+
+    await wait(1000);
+    spinner.succeed();
+    await wait(500);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _getPrompt() {
+    const fix = configOverrides.getFix();
+    if (fix === undefined) {
+      return 'override ESLint --fix';
+    } else if (!fix) {
+      return `toggle ESLint --fix ${chalk.italic('(disabled)')}`;
+    }
+    return `toggle ESLint --fix ${chalk.italic('(enabled)')}`;
   }
 }
 
