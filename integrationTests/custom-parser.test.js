5bb91e7d60745ce43efd97c50ef1b948a992cd47
Mark Wheeler <mark.wheeler@gusto.com>
2019-09-22 09:40:10 -0700

    run travis matrix over ESLint versions

--- integrationTests/custom-parser.test.js
+++ integrationTests/custom-parser.test.js
@@ -1,5 +1,21 @@
+const eslint = require('eslint');
 const runJest = require('./runJest');
 
-it("Doesn't override parser when not set", () => {
-  return expect(runJest('custom-parser')).resolves.toMatchSnapshot();
-});
+// Note: ESLint versions <6 have a different error message for this test. The
+// snapshot file contains both messages so we can test across both versions.
+// Without the skipped tests for the "other" version, the tests will always fail
+// with `1 snapshot obsolete`.
+if (
+  eslint.CLIEngine.version.startsWith('4') ||
+  eslint.CLIEngine.version.startsWith('5')
+) {
+  it.skip("Doesn't override parser when not set", () => {});
+  it("Doesn't override parser when not set [ESLint<6]", () => {
+    return expect(runJest('custom-parser')).resolves.toMatchSnapshot();
+  });
+} else {
+  it("Doesn't override parser when not set", () => {
+    return expect(runJest('custom-parser')).resolves.toMatchSnapshot();
+  });
+  it.skip("Doesn't override parser when not set [ESLint<6]", () => {});
+}
