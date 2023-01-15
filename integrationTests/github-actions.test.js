40d19b3344d9630274246588558e44f123882cf9
Connor Prussin <connor@prussin.net>
2023-01-15 12:39:14 -0800

    Account for lower versions of jest & eslint in integration tests

--- integrationTests/github-actions.test.js
+++ integrationTests/github-actions.test.js
@@ -1,8 +1,16 @@
+const { version } = require('jest/package.json');
+const semver = require('semver');
 const runJest = require('./runJest');
 
-it('Reports with the github actions reporter', async () => {
-  const priorActionsConfig = process.env.GITHUB_ACTIONS;
-  process.env.GITHUB_ACTIONS = true;
-  expect(await runJest('github-actions')).toMatchSnapshot();
-  process.env.GITHUB_ACTIONS = priorActionsConfig;
-});
+// NOTE: Jest versions <28 don't have a github action reporter, so this test is
+// not valid.
+if (semver.satisfies(version, '<28')) {
+  it.skip('Reports with the github actions reporter');
+} else {
+  it('Reports with the github actions reporter', async () => {
+    const priorActionsConfig = process.env.GITHUB_ACTIONS;
+    process.env.GITHUB_ACTIONS = true;
+    expect(await runJest('github-actions')).toMatchSnapshot();
+    process.env.GITHUB_ACTIONS = priorActionsConfig;
+  });
+}
