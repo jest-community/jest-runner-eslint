c4b2b9dd9fe94b1a01b679ed7a5da8e5355e60d5
Connor Prussin <connor@prussin.net>
2023-04-27 00:15:42 -0700

    Remove broken snapshots & tests for unsupported eslint versions

--- integrationTests/custom-parser.test.js
+++ integrationTests/custom-parser.test.js
@@ -2,28 +2,12 @@ const { version } = require('eslint/package.json');
 const semver = require('semver');
 const runJest = require('./runJest');
 
-// Note: ESLint versions <6, 6, 7, >7 each have a different error message or
-// stack for this test. The snapshot file contains all messages so we can test
-// across versions.  Without the skipped tests for the other versions, the tests
-// will always fail with `1 snapshot obsolete`.
-if (semver.satisfies(version, '<6')) {
+// Note: ESLint versions 7 and 8 have a different error message or stack for
+// this test. The snapshot file contains all messages so we can test across
+// versions.  Without the skipped tests for the other versions, the tests will
+// always fail with `1 snapshot obsolete`.
+if (semver.satisfies(version, '7')) {
   it.skip("Doesn't override parser when not set", () => {});
-  it.skip("Doesn't override parser when not set [ESLint=6]", () => {});
-  it.skip("Doesn't override parser when not set [ESLint=7]", () => {});
-  it("Doesn't override parser when not set [ESLint<6]", async () => {
-    expect(await runJest('custom-parser')).toMatchSnapshot();
-  });
-} else if (semver.satisfies(version, '6')) {
-  it.skip("Doesn't override parser when not set", () => {});
-  it.skip("Doesn't override parser when not set [ESLint<6]", () => {});
-  it.skip("Doesn't override parser when not set [ESLint=7]", () => {});
-  it("Doesn't override parser when not set [ESLint=6]", async () => {
-    expect(await runJest('custom-parser')).toMatchSnapshot();
-  });
-} else if (semver.satisfies(version, '7')) {
-  it.skip("Doesn't override parser when not set", () => {});
-  it.skip("Doesn't override parser when not set [ESLint<6]", () => {});
-  it.skip("Doesn't override parser when not set [ESLint=6]", () => {});
   it("Doesn't override parser when not set [ESLint=7]", async () => {
     expect(await runJest('custom-parser')).toMatchSnapshot();
   });
@@ -31,7 +15,5 @@ if (semver.satisfies(version, '<6')) {
   it("Doesn't override parser when not set", async () => {
     expect(await runJest('custom-parser')).toMatchSnapshot();
   });
-  it.skip("Doesn't override parser when not set [ESLint<6]", () => {});
-  it.skip("Doesn't override parser when not set [ESLint=6]", () => {});
   it.skip("Doesn't override parser when not set [ESLint=7]", () => {});
 }
